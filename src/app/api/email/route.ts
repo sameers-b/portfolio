bc18227bf9feeee2a206d4d620b85bf92e202f08
Sameer Singh <sameer@shadowcast.io>
2024-03-01 15:08:50 +0530

    email-changes

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,15 +1,15 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_KEY);
+// import { Resend } from "resend";
+// const resend = new Resend(process.env.RESEND_KEY);
 
 export async function POST(req: Request) {
   const { name, email, message } = await req.json();
-  await resend.sendEmail({
-    from: "Acme <onboarding@resend.dev>",
-    to: "sameersingh7458@gmail.com",
-    subject: "hello world",
-    text: JSON.stringify({ name, email, message }),
-  });
+  // await resend.sendEmail({
+  //   from: "Acme <onboarding@resend.dev>",
+  //   to: "sameersingh7458@gmail.com",
+  //   subject: "hello world",
+  //   text: JSON.stringify({ name, email, message }),
+  // });
 
   return NextResponse.json({
     status: "ok",
